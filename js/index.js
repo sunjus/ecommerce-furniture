e6dc0d469133f3f6e59ed28e53d5b8d264c83ed0
sunjus <sunju.shin1108@gmail.com>
2021-08-16 23:30:52 +0300

    Created fix-nav func

--- js/index.js
+++ js/index.js
@@ -1,8 +1,45 @@
 const hamburger = document.querySelector(".hamburger");
 const navList = document.querySelector(".nav-list");
+const navBar = document.querySelector(".nav");
 
 if (hamburger) {
   hamburger.addEventListener("click", () => {
     navList.classList.toggle("open");
   });
 }
+
+//Fix Nav
+const navHeight = navBar.getBoundingClientRect().height;
+
+window.addEventListener("scroll", () => {
+  const scrollHeight = window.pageYOffset;
+  if (scrollHeight > navHeight) {
+    navBar.classList.add("fix-nav");
+  } else {
+    navBar.classList.remove("fix-nav");
+  }
+});
+
+//Scroll To
+const links = [...document.querySelectorAll(".scroll-link")];
+
+links.map((link) => {
+  if (!link) return;
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const id = e.target.getAttribute("href").slice(1);
+
+    const el = document.getElementsById(id);
+    let position = el.offsetTop - navHeight;
+
+    window.scrollTo({
+      top: position,
+      left: 0,
+    });
+
+    menu.classList.remove("show");
+    document.body.classList.remove("show");
+    navBar.classList.remove("show");
+  });
+});
