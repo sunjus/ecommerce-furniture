09d9e8f801d0adfb5e599eb671a1f114b41f6ec3
sunjus <sunju.shin1108@gmail.com>
2021-08-17 18:58:26 +0300

    Created popup

--- js/index.js
+++ js/index.js
@@ -20,26 +20,18 @@ window.addEventListener("scroll", () => {
   }
 });
 
-//Scroll To
-const links = [...document.querySelectorAll(".scroll-link")];
+//Pop up
+const popup = document.querySelector(".popup");
+const closePopup = document.querySelector(".popup-close");
 
-links.map((link) => {
-  if (!link) return;
-  link.addEventListener("click", (e) => {
-    e.preventDefault();
-
-    const id = e.target.getAttribute("href").slice(1);
-
-    const el = document.getElementsById(id);
-    let position = el.offsetTop - navHeight;
-
-    window.scrollTo({
-      top: position,
-      left: 0,
-    });
-
-    menu.classList.remove("show");
-    document.body.classList.remove("show");
-    navBar.classList.remove("show");
+if (popup) {
+  closePopup.addEventListener("click", () => {
+    popup.classList.add("hide-popup");
   });
-});
+
+  window.addEventListener("load", () => {
+    setTimeout(() => {
+      popup.classList.remove("hide-popup");
+    }, 1000);
+  });
+}
